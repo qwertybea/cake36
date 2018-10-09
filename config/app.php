8e3cc256735728922e1039660e6525cff1ca9c32
Justin Fofana <justin.fofana@gmaill.com>
2018-10-09 18:29:18 -0400

    tried to change database to sqlite, didnt work so well

--- config/app.php
+++ config/app.php
@@ -253,7 +253,7 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
+            'driver' => 'Cake\Database\Driver\Sqlite',
             'persistent' => false,
             'host' => 'localhost',
             /*
@@ -264,7 +264,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => 'mysql',
-            'database' => 'app_int',
+            'database' => ROOT . DS . 'sqlite' . DS . 'default.sqlite',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
